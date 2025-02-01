2152695b8fd6ff5a1c99a61c0a63923a480f4a57
Raul Gamero <raul.a.gamero@gmail.com>
2025-02-01 12:27:10 +0100

    updating the react-countries project

--- react-countries/src/App.jsx
+++ react-countries/src/App.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Countries from './components/Countries';
-import Country from './components/Country';
-import './styles/main.css';
-import './styles/navbar.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";  
+import Countries from "./pages/Countries";  
+import Country from "./pages/Country"; 
 
 function App() {
   return (
     <Router>
-      {/* Setup the routes for each page */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/countries" element={<Countries />} />

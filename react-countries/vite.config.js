8803b5c327d17c24229da453f6a57bd206e05351
Raul Gamero <raul.a.gamero@gmail.com>
2025-02-01 13:21:48 +0100

    updating vite.config.js

--- react-countries/vite.config.js
+++ react-countries/vite.config.js
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, 
+    port: 3000,
     strictPort: true,
-    host: true,
+    fs: {
+      allow: ['.']
+    }
   },
   build: {
-    outDir: 'build', 
+    outDir: 'build',
+    emptyOutDir: true 
   }
 });
+
+
